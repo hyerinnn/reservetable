70a53a828b503b7e0c257f010adbc1670755bec1
hyerinnn <eun8229@gmail.com>
2024-02-20 01:39:15 +0900

    merge

--- src/main/java/my/reservetable/entity/Member.java
+++ src/main/java/my/reservetable/entity/Member.java
@@ -24,7 +24,7 @@ public class Member {
 
     @Column(nullable = false)
     private String email;
-
+    
 
     public Member(String userName, String phoneNumber, String email) {
         this.userName = userName;
