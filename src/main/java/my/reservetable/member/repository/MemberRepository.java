2d84fd86814c63975a79d1c2edadc26d3d34fd2b
hyerinnn <eun8229@gmail.com>
2024-02-26 23:15:32 +0900

    불필요 파일 삭제

--- src/main/java/my/reservetable/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package my.reservetable.member.repository;
-
-import my.reservetable.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- src/main/java/my/reservetable/tmp/ShopRepository.java
+++
@@ -1,8 +0,0 @@
-package my.reservetable.tmp;
-
-import my.reservetable.tmp.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
