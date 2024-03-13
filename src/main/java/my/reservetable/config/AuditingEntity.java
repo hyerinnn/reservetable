290cfec538fccb66b43195b4c566c75fa4aa7f69
hyerinnn <eun8229@gmail.com>
2024-03-14 03:19:36 +0900

    생성일, 수정일에 DateTimeFormat 추가

--- src/main/java/my/reservetable/config/AuditingEntity.java
+++ src/main/java/my/reservetable/config/AuditingEntity.java
@@ -9,7 +9,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
@@ -19,20 +18,20 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)  //Spring Data JPA에서 제공하는 이벤트 리스너로 엔티티의 영속, 수정 이벤트를 감지하는 역할
 public class AuditingEntity {
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;     //생성일
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime modifiedDate;   //수정일
 
-
     @CreatedBy
     @Column(nullable = false, updatable = false, length = 100)
     private String createdBy;            //생성자
-
-
+    
     @LastModifiedBy
     @Column(nullable = false, length = 100)
     private String modifiedBy;          //수정자
