332e2a42ac0a9c2a68b46724072accfbeba19f37
hyerinnn <eun8229@gmail.com>
2024-04-04 00:08:28 +0900

    테스트와 로컬 환경 분리, 로컬환경에서는 data.sql 적용

---
+++ src/test/java/my/reservetable/IntegrationTestSupport.java
@@ -0,0 +1,11 @@
+package my.reservetable;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+public abstract class IntegrationTestSupport {
+}
