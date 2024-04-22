a3a3c00ac87f4581dfc6d80f1b4c76dbe88fa92e
hyerinnn <eun8229@gmail.com>
2024-04-23 04:20:20 +0900

    [refactor] data.sql 내 owner테이블 데이터 insert 부분 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,13 +7,6 @@ insert into MEMBER (role, nick_name, password, phone_number, email, created_by, 
 values ('USER','유저계정','$2a$10$nyOJzE5TYbAIpKXCx5ST6OqYM.PKxvZeRPpdqsZHWm7DLb5/zBfw.', '01048452111','user@user.com','user', '2024-04-03 18:50:15.666609','user', '2024-04-03 18:50:15.666609');
 
 
-
-insert into owner (nick_name, password, phone_number, email, created_by, created_date,modified_by, modified_date)
-    values ('분식집오너','1234', '01011112222','owner001@owner001.com','owner001', '2024-04-03 18:50:15.666609','owner001','2024-04-03 18:50:15.666609');
-insert into owner (nick_name, password, phone_number, email, created_by, created_date,modified_by, modified_date)
-    values ('중화루','gdg848', '01048452111','owner002@owner002.com','owner002', '2024-04-03 18:50:15.666609','owner002', '2024-04-03 18:50:15.666609');
-
-
 insert into shop (shop_name, member_id, shop_number, zipcode,address1,address2,country_category,
                   status,description, open_time, last_order_time, waiting_yn, created_by, created_date,modified_by, modified_date)
     values ('김가네', 1, '021112222','15481','서울특별시 강남구', null,'KOREAN', 'OPEN', '분식집입니다.','10:00:00','21:00:00','Y',
