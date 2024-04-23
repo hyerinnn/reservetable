61fd685b3810a4371fd26dc0fd6eafb13356fec8
hyerinnn <eun8229@gmail.com>
2024-04-24 00:50:47 +0900

    [Test] 테스트용 shop 더미데이터 추가

--- src/main/java/my/reservetable/comon/dummy/DummyObject.java
+++ src/main/java/my/reservetable/comon/dummy/DummyObject.java
@@ -2,7 +2,13 @@ package my.reservetable.comon.dummy;
 
 import my.reservetable.member.domain.Member;
 import my.reservetable.member.domain.MemberRole;
+import my.reservetable.shop.domain.Address;
+import my.reservetable.shop.domain.Shop;
+import my.reservetable.shop.domain.ShopCountryCategory;
+import my.reservetable.shop.domain.ShopStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalTime;
 
 public class DummyObject {
 
@@ -28,6 +34,22 @@ public class DummyObject {
                 .nickName("새로운회원")
                 .phoneNumber("01012345678")
                 .role(role)
+                .build();
+    }
+
+    protected Shop createShop(Member ownerMember, String description, ShopCountryCategory countryCategory,
+                            ShopStatus status, LocalTime openTime, LocalTime lastOrderTime, String waitingYn){
+        return Shop.builder()
+                .shopName("뉴레스토랑")
+                .member(ownerMember)
+                .shopNumber("02-1234-5678")
+                .address(new Address("15151","서울특별시 00로 32"))
+                .description(description)
+                .countryCategory(countryCategory)
+                .status(status)
+                .openTime(openTime)
+                .lastOrderTime(lastOrderTime)
+                .waitingYn(waitingYn)
                 .build();
     }
 }
