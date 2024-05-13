2caa5eb6372131e29b4df9fd3955f3ef344764b4
hyerinnn <eun8229@gmail.com>
2024-05-14 05:26:02 +0900

    [refactor] 웨이팅 미루기 구현 진행중 (임시커밋)
    웨이팅 미루기  기능 구현을 위해,  기존 웨이팅 로직에 수정이 필요하여 임시 커밋

--- src/main/java/my/reservetable/waiting/service/WaitingService.java
+++ src/main/java/my/reservetable/waiting/service/WaitingService.java
@@ -2,6 +2,8 @@ package my.reservetable.waiting.service;
 
 import lombok.RequiredArgsConstructor;
 import my.reservetable.exception.NotFoundEntityException;
+import my.reservetable.point.domain.PointType;
+import my.reservetable.point.service.PointService;
 import my.reservetable.waiting.domain.Waiting;
 import my.reservetable.waiting.domain.WaitingStatus;
 import my.reservetable.waiting.dto.response.WaitingResponse;
@@ -15,25 +17,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class WaitingService {
 
     private final WaitingRepository waitingRepository;
+    private final PointService pointService;
 
     @Transactional
     public WaitingResponse changeWaitingStatus(Long waitingId, WaitingStatus status) {
-        //TODO : 지난 웨이팅, 입장완료 등의 상태는 변경하면 안되므로, 현재 입장순서 웨이팅에 대해서만 특정 변경할 수 있도록 제한을 걸어야함
 
         Waiting waiting = waitingRepository.findById(waitingId)
                 .orElseThrow(() -> new NotFoundEntityException("웨이팅 정보를 찾을 수 없습니다."));
+        checkWaitingStatusForChange(waiting);
         waiting.changeWaitingStatus(status);
         return WaitingResponse.toDto(waiting);
     }
 
     @Transactional
-    public void 미루기(Long waitingId){
+    public void postponeWaiting(Long waitingId){
         Waiting waiting = waitingRepository.findById(waitingId)
                 .orElseThrow(() -> new NotFoundEntityException("웨이팅 정보를 찾을 수 없습니다."));
 
-        // TODO : 웨이팅 미루기 구현
-        // 현재 진행중인 웨이팅인지 확인
+        checkWaitingStatusForChange(waiting);
         // 웨이팅 미루기
-        // 포인트 차감
+        // TODO : 웨이팅 로직 수정필요
+
+        // 포인트차감
+        pointService.subtractPoint(waiting.getMember().getId() ,PointType.GENERAL.getPoint());
+    }
+
+    private void checkWaitingStatusForChange(Waiting waiting){
+        //현재 입장대기 웨이팅에 대해서만 변경가능
+        if(!(waiting.getWaitingStatus()).equals(WaitingStatus.READY)){
+            throw new IllegalArgumentException("입장대기 상태의 웨이팅만 변경 할 수 있습니다.");
+        }
     }
 }
